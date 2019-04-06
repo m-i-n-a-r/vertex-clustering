3e07d749f994787b3b9c871c0c5f15a3b520300b
m-i-n-a-r <glucaconti.952@gmail.com>
2019-04-07 01:55:43 +0200

    Better plot, bugs in readme

--- launcher.py
+++ launcher.py
@@ -19,7 +19,7 @@ except:
     print("The number is too big, small or is not a number. The value was set to default.")
 
 try:
-    shingle_size = input("\nShingle size [blank for defult] -> ")
+    shingle_size = input("\nShingle size [blank for default] -> ")
     shingle_size = int(shingle_size)
     if not 5 <= shingle_size <= 50: sys.exit("The number is too big, small or is not a number")
 except:
@@ -27,32 +27,74 @@ except:
     print("The number is too big, small or is not a number. The value was set to default.")
 
 print("\n...computing...\n")
-#try:
-clusters = vertex_clusterer(treshold, shingle_size)
-#except:
-#    sys.exit("Something went wrong: malformed or small pages, internal error.")
+try:
+    clusters = vertex_clusterer(treshold, shingle_size)
+except:
+    print("Something went wrong: malformed or small pages, internal error.")
 
 if not clusters: 
-    sys.exit("No cluster found.")
+    sys.exit("\nNo cluster found or failure.")
 
-# Preparing the coordinates
+# Preparing the coordinates and the colorscale
 json_ready_dict = {}
 pages_in_cluster = []
 shingle_of_cluster = []
+colors = []
 for cluster in clusters:
     pages_in_cluster.append(len(clusters[cluster]))
     json_ready_dict[str(cluster)] = list(clusters[cluster])
     shingle_of_cluster.append(str(cluster))
 
+for i in range(0,len(shingle_of_cluster)): 
+    colors.append(i)
+
 # Preparing the chart
 data = [go.Bar(
     x = shingle_of_cluster,
     y = pages_in_cluster,
-    name = 'Clusters'
+    name = 'Vertex Clustering',
+       marker = {
+        'color': colors,
+        # Colorscale type. Other possible options below
+        # Blackbody Bluered Blues Earth Electric Greens Greys Hot Jet Picnic Portland Rainbow RdBu Reds Viridis YlGnBu YlOrRd
+        'colorscale': 'Viridis' 
+    }
 )]
 
+config = {
+    'scrollZoom': True,
+    'displaylogo': False,
+    'watermark': False
+    }
+
+layout = go.Layout(
+    title = 'Vertex Clustering',
+    xaxis = dict(
+        title = 'CLUSTERS - TOTAL: ' + str(len(colors)),
+            titlefont = dict(
+            family = 'Arial, sans-serif',
+            size = 18,
+            color = '#777777'
+        ),
+        showticklabels = False,
+        rangemode = 'nonnegative',
+        autorange = True
+    ),
+    yaxis = dict(
+        title = 'NUMBER OF PAGES',
+            titlefont = dict(
+            family = 'Arial, sans-serif',
+            size = 18,
+            color = '#777777'
+        ),
+        rangemode = 'nonnegative',
+        autorange = True
+    ), 
+)
+
 # Printing and saving the chart
-py.offline.plot(data, filename='clusters.html')
+fig = go.Figure(data = data, layout = layout)
+py.offline.plot(fig, filename = 'clusters.html', config = config)
 
 # Write the clusters in a json file
 json_ready_dict = {'Clusters': json_ready_dict}
@@ -60,4 +102,4 @@ with open('clusters.json', 'w') as file:
      file.write(json.dumps(json_ready_dict))
 
 # All done, confirmation message
-print("\nDone. The plot is in the current folder.")
+print("\nDone. The plot is in the current folder, as well as a json containing every cluster and its set of pages.\n")
